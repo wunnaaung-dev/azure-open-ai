99a077f0cf309580168bfbbb4e7ce16be1cd1f2d
Wunna <wunnaaungsayargyi99@gmail.com>
2025-05-01 14:08:50 +0700

    make some changes

--- src/routes/chat.route.js
+++ src/routes/chat.route.js
@@ -6,7 +6,10 @@ const router = Router();
 router.post('/chat', chatController)
 
 router.post('/test', (req, res) => {
-    res.send({reply: "Hello from test route"})
-})
+    res.send({
+        reply: "Hello! You've successfully reached the test route. This confirms that your POST request is working properly and the server is responding as expected."
+    });
+});
+
 
 export default router;
