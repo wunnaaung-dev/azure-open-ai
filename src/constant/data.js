6b5c94036f1d97b2519dcfb5ba0cc6f21f4295f0
Wunna <wunnaaungsayargyi99@gmail.com>
2025-05-01 11:17:49 +0700

    add new data

--- src/constant/data.js
+++ src/constant/data.js
@@ -4,23 +4,31 @@ export const mySelf = {
     dob: "6th January 1999",
     gender: "Male",
     birthPlace: "Kanbalu Township, Sagaing Region, Myanmar",
+    quote: "If I die today, I don't want to regret the way of my living. So, I try, I grind, and I fly",
+    personality: "Ambivert | ENFP",
+    values: ["Honesty", "Curiosity", "Growth mindset"],
     height: "5'11\"",
     contacts: {
         phone: "+66 88413973",
         email: "wunnaaung1999.dev@gmail.com",
         linkedin: "https://www.linkedin.com/in/wunna-aung-256116227/",
         github: "https://github.com/wunnaaung-dev",
-        resume: "https://docs.google.com/document/d/1vZny3WOgH5shPieFImSyMQXqwQmi68kE/edit?usp=sharing&ouid=110943460216350641613&rtpof=true&sd=true"
+        resume: "https://docs.google.com/document/d/1vZny3WOgH5shPieFImSyMQXqwQmi68kE/edit?usp=sharing&ouid=110943460216350641613&rtpof=true&sd=true",
+        instagram: "https://www.instagram.com/wuxx9968?igsh=bjJuM3liZmxsNzg4"
     },
     relationshipStatus: "Single",
     favourites: {
         color: ["White", "Black"],
         sport: ["Football", "Volleyball"],
         music: ["Hip Hop", "Trap", "Drill", "Jersey"],
+        artists: "Avicii",
+        songs: ["Levels", "The Nights"],
         movie: ["Peaky Blinders", "Money Heist", "Harry Potter"],
-        anime: ["Haikyuu!!", "Jujutsu Kaisen", "Your Name", ],
-        food: ["ချိုချဉ်ကြော်", "မုန့်ဟင်းခါး",],
+        anime: ["Haikyuu!!", "Jujutsu Kaisen", "Your Name",],
+        character: ["Thomas Shelby", "Bokuto"],
+        food: ["ချိုချဉ်ကြော်", "မုန့်ဟင်းခါး", "လက်ဖက်သုပ်"],
         coffee: ["Mocha", "Latte"],
+        tea: ["ပုံမှန်", "ကျစိမ့်", "ချိုစိမ့်"]
     },
     occupation: "Software Engineer | Full Stack Developer",
     hobbies: ["Coding", "Reading", "Listening to music", "Gym"],
@@ -35,15 +43,33 @@ export const mySelf = {
             matriculationMarks: 513,
             distinctionSubjects: ["Myanmar", "Mathematics", "Physics", "Chemistry", "Biology"],
         },
-        university: {
-            name: "Yangon Technological University (RIT | YIT | YTU)",
-            specialization: "Metallurgy and Materials Engineering",
+        universities: {
+            2015_2020: {
+                name: "Yangon Technological University (RIT | YIT | YTU)",
+                specialization: "Metallurgy and Materials Engineering",
+                remark: "Because of COVID-19, I couldn't continue my study.",
+            },
+            2022_2024: {
+                name: "GUSTO University",
+                specialization: "Computing",
+                course_works: ["Programming", "Networking", "Database Design and Development", "Professional Practice", "Web Development", "Seurity", "Project Management", "Business Intelligence", "Maths for Computing", "Cloud Computing", "Internet of Things", "Advanced Programming", "Computing Research Project", "Discrete Mathematics", "Data Structures and Alogorithms"],
+            },
         }
     },
     education: {
-        degree: "Bachelor of Sciency in Computer Systems Engineering",
+        degree: "Bachelor of Science in Computer Systems Engineering",
         university: "University of Sunderland",
+        course_works: ["Android Development", "Artificail Intelligence", "Advanced Cybersecurity", "Advanced Data Technologies", "Research", "Product Development"],
         graduationYear: 2025
     },
-    skills: ["JavaScript", "React", "Node.js", "Express.js"]
+    skills: ["JavaScript", "TypeScript", "React", "NEXT.js", "Node.js", "Express.js", "RESTful API", "SQL", "MongoDB", "AWS"],
+    favouriteTechnologies: ["React", "Tanstack Query", "Redux Toolkit", "AWS"],
+    funFacts: [
+        "I can dance all night without using drugs and code the next day",
+        "If you want to know my personality, just look at Bokuto from Haikyuu!!",
+        "My longest relationship lasted one month. lol",
+        "Except me and my brothers, other men are ခွေးမသား 😎",
+        "When we drink, we say: 'For the girls we love, for the girls we lost' instead of just 'cheers'"
+    ],
+    alterEgo: "Bokuto from Haikyuu!! (but with more coffee and code)"
 }
